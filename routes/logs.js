ab3dfab127c6672fa0e424f40e9fc5583e82648d
PaulinaLL <paulinalll90@wp.pl>
2020-03-11 15:32:50 +0100

    delate unnecessary lines

--- routes/logs.js
+++ routes/logs.js
@@ -1,6 +1,3 @@
-const express = require("express");
-const app = express();
-
 const logs = function(req, res, next) {
   console.log("Method:", req.method, "On route:", req.originalUrl);
   if (req.body) {
