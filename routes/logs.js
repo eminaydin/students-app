b678139196ef482cd25d62fdb9bcec3ff43e0b26
PaulinaLL <paulinalll90@wp.pl>
2020-03-11 14:36:43 +0100

    create logger middleware

---
+++ routes/logs.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const logs = function(req, res, next) {
+  console.log("Method:", req.method, "On route:", req.originalUrl);
+  if (req.body) {
+    console.log("payload:", req.body);
+  }
+  next();
+};
+
+module.exports = logs;
