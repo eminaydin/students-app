081f54df470a9a06ba3262face094883c0e2f197
agumusay <a.gumusay@protonmail.com>
2020-03-17 21:23:22 +0100

    Students api test

--- tests/students.test.js
+++ tests/students.test.js
@@ -2,11 +2,18 @@ const request = require("supertest");
 const app = require("../app");
 
 describe("Testing GET api/students", () => {
-  test("It should respond with status code -> 200, Content-Type -> JSON, Content is an array", async () => {
+  test("Content-Type -> JSON", async () => {
+    const response = await request(app).get("/api/students");
+    expect.objectContaining({
+      headers: expect.objectContaining({ "Content-Type": "application/json" })
+    });
+  });
+  test("It should respond with status code -> 200", async () => {
+    const response = await request(app).get("/api/students");
+    expect(response.statusCode).toBe(200);
+  });
+  test("Content is an array", async () => {
     const response = await request(app).get("/api/students");
     expect(Array.isArray(response.body)).toBe(true);
-    expect(response.body.length).toBe(1);
-    expect(response.body[0]).toHaveProperty("name");
-    expect(response.statusCode).toBe(200);
   });
 });
