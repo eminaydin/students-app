81d043d74b14c132c138c6291a797d2a91a2c025
PaulinaLL <paulinalll90@wp.pl>
2020-03-17 22:15:03 +0100

    delate empty line

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-
 const app = express();
 
 // Routes
 const studentRoutes = require("./routes/students");
 
 app.use(express.json());
+
 app.use("/api/students", studentRoutes);
 
 module.exports = app;
