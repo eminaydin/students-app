4331bbdb794094878bdf6eac46addfc30aab0a95
Oliver Weber <oliver@mondough.io>
2020-03-18 16:14:26 +0100

    some adoptions

--- tests/students.test.js
+++ tests/students.test.js
@@ -4,14 +4,16 @@ const app = require("../app");
 describe("Testing GET api/students", () => {
   test("Content-Type -> JSON", async () => {
     const response = await request(app).get("/api/students");
-    expect.objectContaining({
-      headers: expect.objectContaining({ "Content-Type": "application/json" })
-    });
+    const expectedCase = "application/json; charset=utf-8";
+
+    expect(response.headers["content-type"]).toBe(expectedCase);
   });
+
   test("It should respond with status code -> 200", async () => {
     const response = await request(app).get("/api/students");
     expect(response.statusCode).toBe(200);
   });
+
   test("Content is an array", async () => {
     const response = await request(app).get("/api/students");
     expect(Array.isArray(response.body)).toBe(true);
