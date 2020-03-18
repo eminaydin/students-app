18a80e5125394b542987535ba51e5fc75ceb2c0d
agumusay <a.gumusay@protonmail.com>
2020-03-18 17:04:40 +0100

    fix lines

--- tests/students.test.js
+++ tests/students.test.js
@@ -1,6 +1,25 @@
 const request = require("supertest");
 const app = require("../app");
 
+describe("Testing GET api/students", () => {
+  test("Content-Type -> JSON", async () => {
+    const response = await request(app).get("/api/students");
+    const expectedCase = "application/json; charset=utf-8";
+
+    expect(response.headers["content-type"]).toBe(expectedCase);
+  });
+
+  test("It should respond with status code -> 200", async () => {
+    const response = await request(app).get("/api/students");
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("Content is an array", async () => {
+    const response = await request(app).get("/api/students");
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+});
+
 describe("Testing POST api/students", () => {
   test("Content-Type -> JSON", async () => {
     const newStudent = await request(app)
@@ -25,24 +44,5 @@ describe("Testing POST api/students", () => {
         location: "BER"
       })
     );
-  });
-});
-
-describe("Testing GET api/students", () => {
-  test("Content-Type -> JSON", async () => {
-    const response = await request(app).get("/api/students");
-    const expectedCase = "application/json; charset=utf-8";
-
-    expect(response.headers["content-type"]).toBe(expectedCase);
-  });
-
-  test("It should respond with status code -> 200", async () => {
-    const response = await request(app).get("/api/students");
-    expect(response.statusCode).toBe(200);
-  });
-
-  test("Content is an array", async () => {
-    const response = await request(app).get("/api/students");
-    expect(Array.isArray(response.body)).toBe(true);
   });
 });
