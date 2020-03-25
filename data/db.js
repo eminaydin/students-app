cf479d261547eb779d6393d7cba1b7a19efa9f9c
agumusay <a.gumusay@protonmail.com>
2020-03-25 20:10:29 +0100

    I should have made different branches

--- data/db.js
+++ data/db.js
@@ -1,26 +1,78 @@
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 
-const adapter = new FileSync("data/db.json");
+const adapter = new FileSync("./data/db.json");
 const db = low(adapter);
 
-const exampleStudent = {
-  name: "Rupert",
-  lastname: "Jalili",
-  age: 30,
-  class: "FBW101",
-  location: "BER"
-};
+const exampleStudents = [
+  {
+    name: "Rupert",
+    lastname: "Jalili",
+    age: 30,
+    class: "FBW101",
+    location: "BER"
+  },
+  {
+    name: "Rupertilla",
+    lastname: "Jalili",
+    age: 28,
+    class: "FBW101",
+    location: "BER"
+  }
+];
 
 // default setup
 db.defaults({
-  students: [exampleStudent]
+  students: [...exampleStudents]
 }).write();
 
+console.log(
+  db
+    .get("students")
+    .push({
+      name: "Ruperzilla",
+      lastname: "Zillage",
+      age: 500,
+      class: "GS1905",
+      location: "Galatasaray"
+    })
+    .write()
+);
 module.exports = {
   // TODO: get all students
+
+  getStudents: () => {
+    let students = db.get("students").value();
+    return students;
+  },
+
   // TODO: get by name
+
+  getStudentByName: name => {
+    db.get("students")
+      .find({ name: name })
+      .value();
+  },
+
   // TODO: update by name
+
+  updateStudentByName: name => {
+    db.get("students")
+      .find({ name: name })
+      .assign({ name: "Ruperzolla", lastname: "Radiator", age: 500 })
+      .write();
+  },
   // TODO: delete by name
+
+  deleteStudentByName: name => {
+    db.get("students")
+      .remove({ name: name })
+      .write();
+  },
   // TODO: add new student
+  addStudent: studentObject => {
+    db.get("students")
+      .push(studentObject)
+      .write();
+  }
 };
