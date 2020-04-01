c7fdc8c95f3ed80c8ab5b8d7b7534637f45696dc
geraldjän <solidcaius@gmail.com>
2020-04-01 09:44:35 +0200

    added controller folder and tried students.js

---
+++ controller/students.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const path = require("path");
+const studentsDataPath = path.join(__dirname, "../data/students.json");
+const validation = require("../middleware/validation");
+
+
+const getAllStudents = (req,res) => {
+    fs.readFile(studentsDataPath, "utf-8", (err,data) => {
+        console.log(req);
+    })
+}
+
+console.log(getAllStudents());
+
+module.exports = {
+    getAllStudents
+} 
