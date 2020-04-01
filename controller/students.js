e9a7fd92a514844d89a910230fdd4da983755b13
agumusay <a.gumusay@protonmail.com>
2020-04-01 10:44:27 +0200

    lowdb in controller

---
+++ controller/students.js
@@ -0,0 +1,69 @@
+const low = require("lowdb");
+const path = require("path");
+const studentsPath = path.join(__dirname, "../data/students.json");
+console.log(studentsPath);
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync(studentsPath);
+const db = low(adapter);
+
+// default setup
+db.defaults({
+  students: [studentsPath]
+}).write();
+
+module.exports = {
+  // TODO: get all students
+
+  getStudents: (req, res) => {
+    let students = db.get("students").value();
+    console.log(students);
+    res.status(200).json(students);
+  },
+
+  // TODO: get by name
+
+  getStudentByName: (req, res) => {
+    let foundStudent = db
+      .get("students")
+      .find({ name: req.params.name })
+      .value();
+    if (foundStudent) {
+      return res.status(200).json(foundStudent);
+    }
+    res.status(404).json({ error: "Student Not Found" });
+  },
+
+  // TODO: update by name
+
+  updateStudentByName: (req, res) => {
+    let updatedStudent = db
+      .get("students")
+      .find({ name: req.params.name })
+      .assign(req.body)
+      .write();
+    res.send(updatedStudent);
+  },
+
+  // TODO: remove by name
+
+  removeStudentByName: (req, res) => {
+    let removedStudent = db
+      .get("students")
+      .remove({ name: req.params.name })
+      .write();
+    res.send(removedStudent);
+  },
+
+  // TODO: add new student
+
+  addStudent: (req, res) => {
+    let addedStudent = db
+      .get("students")
+      .push(req.body)
+      .write();
+    if (addedStudent) {
+      res.send(req.body);
+    }
+    res.send("Not a valid student");
+  }
+};
--- routes/students.js
+++ routes/students.js
@@ -1,79 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
-const path = require("path");
-const studentsDataPath = path.join(__dirname, "../data/students.json");
+
 const validation = require("../middleware/validation");
+const {
+  getStudents,
+  getStudentByName,
+  updateStudentByName,
+  removeStudentByName,
+  addStudent
+} = require("../controller/students");
 
 // - GET (all, individual)
 
-router.get("/", (req, res) => {
-  fs.readFile(studentsDataPath, "utf-8", (err, data) => {
-    if (err) throw err;
-    res.status(200).json(JSON.parse(data));
-  });
-});
+router.get("/", getStudents);
 
-router.get("/:name", (req, res) => {
-  fs.readFile(studentsDataPath, "utf-8", (err, data) => {
-    if (err) console.log(err);
-    data = JSON.parse(data);
-    const student = data.find(
-      ({ name }) => name.toLowerCase() === req.params.name.toLowerCase()
-    );
-
-    if (student) {
-      return res.status(200).json(student);
-    }
-    res.status(404).json({ error: "Student not found" });
-  });
-});
+router.get("/:name", getStudentByName);
 
 // - PUT (individual)
-router.put("/:name", validation, (req, res) => {
-  let students = fs.readFileSync(studentsDataPath, "utf-8");
-  students = JSON.parse(students);
-  if (req.params.name && req.body) {
-    students = students.map((student) => {
-      if (student.name.toLowerCase() === req.params.name.toLowerCase()) {
-        Object.assign(student, req.body);
-      }
-
-      return student;
-    });
-  }
-  fs.writeFileSync(studentsDataPath, JSON.stringify(students));
-  res.send(students);
-});
+router.put("/:name", validation, updateStudentByName);
 // // - DELETE (individual)
-router.delete("/:name", (req, res) => {
-  let students = fs.readFileSync(studentsDataPath, "utf-8");
-  students = JSON.parse(students);
-
-  if (req.params.name) {
-    students = students.filter(
-      ({ name }) => name.toLowerCase() !== req.params.name.toLowerCase()
-    );
-    fs.writeFileSync(studentsDataPath, JSON.stringify(students));
-  }
-
-  res.send(students);
-});
+router.delete("/:name", removeStudentByName);
 // - POST (individual)
-router.post("/", validation, (req, res) => {
-  let students = fs.readFileSync(studentsDataPath, "utf-8");
-  students = JSON.parse(students);
-  students.push(req.body);
-
-  if (students) {
-    fs.writeFileSync(studentsDataPath, JSON.stringify(students));
-    return res.send({
-      status: "success",
-      message: `student with name: ${req.body.name} added`
-    });
-  }
-
-  res.send("NO!");
-});
+router.post("/", validation, addStudent);
 
 module.exports = router;
