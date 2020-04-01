b7fb46cc651dfb60b3150ccc59064c8e01eb64ea
geraldjän <solidcaius@gmail.com>
2020-04-01 10:10:31 +0200

    refered in routes to controller

--- controller/students.js
+++ controller/students.js
@@ -1,10 +1,7 @@
 const express = require("express");
-const router = express.Router();
 const fs = require("fs");
 const path = require("path");
 const studentsDataPath = path.join(__dirname, "../data/students.json");
-const validation = require("../middleware/validation");
-
 
 const getAllStudents = (req,res) => {
     fs.readFile(studentsDataPath, "utf-8", (err,data) => {
