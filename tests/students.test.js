fa824e7559fc49240b14b89483460ae28cda692f
agumusay <a.gumusay@protonmail.com>
2020-04-04 00:49:54 +0200

    remove logs

--- tests/students.test.js
+++ tests/students.test.js
@@ -13,8 +13,8 @@ afterEach(() => {
       lastname: "Jalili",
       age: 30,
       class: "FBW101",
-      location: "BER"
-    }
+      location: "BER",
+    },
   ];
 
   db.set("students", defaultContent).write();
@@ -35,56 +35,49 @@ describe("Testing GET api/students", () => {
 
   test("Content is an array", async () => {
     const response = await request(app).get("/api/students");
-    console.log(response.body);
     expect(Array.isArray(response.body)).toBe(true);
   });
 });
 
 describe("Testing DELETE request on api/students", () => {
   test("Post new Student, DELETE new student", async () => {
-    const newStudent = await request(app)
-      .post("/api/students")
-      .send({
-        name: "TestName",
-        lastname: "TestLastName",
-        age: 22,
-        class: "FBW101",
-        location: "BER"
-      });
+    const newStudent = await request(app).post("/api/students").send({
+      name: "TestName",
+      lastname: "TestLastName",
+      age: 22,
+      class: "FBW101",
+      location: "BER",
+    });
     const removedStudent = await request(app).delete(`/api/students/${newStudent.body.name}`);
     expect(removedStudent.statusCode).toBe(200);
   });
   test("GET updated students aray", async () => {
     const response = await request(app).get(`/api/students/TestName`);
-    console.log(typeof response.body);
     expect(response.body).toEqual({ error: "Student Not Found" });
   });
 });
 
 describe("Testing POST api/students", () => {
   test("Content-Type -> JSON", async () => {
-    const newStudent = await request(app)
-      .post("/api/students")
-      .send({
-        name: "TestName",
-        lastname: "TestLastName",
-        age: 22,
-        class: "FBW101",
-        location: "BER"
-      });
+    const newStudent = await request(app).post("/api/students").send({
+      name: "TestName",
+      lastname: "TestLastName",
+      age: 22,
+      class: "FBW101",
+      location: "BER",
+    });
 
     let expectedCase = "application/json; charset=utf-8";
     expect(newStudent.headers["content-type"]).toBe(expectedCase);
     expect(newStudent.statusCode).toBe(200);
     const response = await request(app).get("/api/students/TestName");
-    console.log(response.body);
     expect(response.body).toEqual(
       expect.objectContaining({
         name: "TestName",
         lastname: "TestLastName",
         age: 22,
         class: "FBW101",
-        location: "BER"
+        location: "BER",
       })
     );
   });
@@ -93,13 +86,11 @@ describe("Testing POST api/students", () => {
 describe("Testing GET api/students/:name", () => {
   test("Content-Type -> JSON", async () => {
     const response = await request(app).get("/api/students/Rupert");
-    console.log(response.body);
     let expectedCase = "application/json; charset=utf-8";
     expect(response.headers["content-type"]).toBe(expectedCase);
   });
   test("It should respond with status code -> 200", async () => {
     const response = await request(app).get("/api/students/Rupert");
-    console.log(response.body);
     expect(response.statusCode).toBe(200);
   });
   test("Content is an Object", async () => {
